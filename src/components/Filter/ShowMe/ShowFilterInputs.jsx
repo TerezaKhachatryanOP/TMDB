d2e6df3018583ce3d2fb8c2bb8f3942eac19052e
Tereza Khachatryan <Tereza.Khachatryan@hearst.com>
2026-02-23 12:55:04 +0400

    added cursor pointer to show-me's inputs

--- src/components/Filter/ShowMe/ShowFilterInputs.jsx
+++ src/components/Filter/ShowMe/ShowFilterInputs.jsx
@@ -1,14 +1,17 @@
+import "../../../styles/FilterStyles/ShowMe.css";
+
 export default function ShowFilterInputs({ value, checked, onChange, name }) {
   return (
     <div className="show-me-input">
       <input
+      className="input-item"
         type="radio"
         name={name}
         value={value}
         checked={checked}
         onChange={onChange}
       />
-      <label htmlFor={value}>{value}</label>
+      <label className="input-item" htmlFor={value}>{value}</label>
     </div>
   );
 }
