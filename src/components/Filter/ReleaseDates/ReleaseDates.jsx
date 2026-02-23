95aac06ca1d6aee38b5f1fdabf87a5066d34cacd
Tereza Khachatryan <Tereza.Khachatryan@hearst.com>
2026-02-23 15:37:19 +0400

    added calendar

--- src/components/Filter/ReleaseDates/ReleaseDates.jsx
+++ src/components/Filter/ReleaseDates/ReleaseDates.jsx
@@ -1,11 +1,13 @@
 import SearchReleases from "./SearchReleases";
 import "../../../styles/FilterStyles/releaseDates.css";
+import DateRangeFields from "./DateRangeFields";
 
 export default function ReleaseDates() {
   return (
     <div className="release-dates-wrapper">
       <h1 className="release-header">Release Dates</h1>
       <SearchReleases />
+      <DateRangeFields />
     </div>
   );
 }
